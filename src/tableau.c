1a6a75de62ea0062d9a12f78957dd0f277c3dc37
Sylvain Jalbert <jalbertsyl@eisti.fr>
2020-03-16 18:12:16 +0100

    développement du jeu et modification bug d'affichage

--- src/tableau.c
+++ src/tableau.c
@@ -22,39 +22,36 @@ void affichage(int ttint_plateau[N][N]){
   int int_j; //variables qui va parcourir les indice des colonnes du tableau
 
   //AFFICHAGE DE LA GRILLE
-  //afficher le décalage
-  printf("\n  ");
-  //affichage des indices colonnes
-  for(int_i = 0 ; int_i < N ; int_i++){
-    printf("  %d ", int_i);
-  }
-  //affichage fin de ligne
-  printf("\n");
   //parcour de chaque lignes
   for(int_i = 0 ; int_i < N ; int_i++){
     //affichage du séparateur
     //affichage du décalage du séparateur
-    printf("  ");
+    printf(" ");
     //affichage du séparateur de longeur N
     for(int_j = 0 ; int_j < N ; int_j++){
-      printf("+---");
+      printf("+----");
     }
     //affichage du dernier caractère du séparateur + retour ligne
     printf("+\n ");
     //parcour de chaque case de la ligne
     for(int_j = 0 ; int_j < N ; int_j++){
       //afficher un séparateur et la valeur de la case
-      printf("| %d ", ttint_plateau[int_i][int_j]);
+      if(ttint_plateau[int_i][int_j]>=10){
+        printf("| %d ", ttint_plateau[int_i][int_j]);
+      }
+      else{
+        printf("|  %d ", ttint_plateau[int_i][int_j]);
+      }
     }
     //afficher le dernier separateur de colonne + fin de la ligne
     printf("|\n");
   }
   //afficher le dernier separateur de ligne
   //affichage du décalage du séparateur
-  printf("  ");
+  printf(" ");
   //affichage du séparateur de longeur N
   for(int_j = 0 ; int_j < N ; int_j++){
-    printf("+---");
+    printf("+----");
   }
   //affichage du dernier caractère du séparateur + retour ligne
   printf("+\n ");
